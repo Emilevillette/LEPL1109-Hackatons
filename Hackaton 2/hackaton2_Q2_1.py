6173ce4ef9b0d5087d457711f6dbcffa497d63b5
Bhasher <github.it@bhasher.com>
2022-10-27 14:58:20 +0200

    Q2.1

---
+++ Hackaton 2/hackaton2_Q2_1.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import pandas as pd
+from datetime import datetime
+from scipy import stats
+
+df = pd.read_csv('Data_energy_load.csv', sep=';', parse_dates=['Date'], skipinitialspace=True)
+df['Date'] = [dt['Date'].replace(hour=dt['Hour']) for i, dt in df.iterrows()]
+df.drop(['Hour'], axis=1, inplace=True)
+
+plt.figure(figsize=(11, 3))
+plt.xlim(df['Date'].min(), df['Date'].max())
+plt.plot(df['Date'], df['Load'], lw=0.1)
+plt.title('Time serie of consumption')
+plt.xlabel('Date')
+plt.ylabel('Power consumption (MW)')
+
+if __name__ == '__main__':
+  plt.show()
